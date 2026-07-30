9044626c0280eca3fba359f3debc553aac0c76fb
zedsu <yigitzedus@gmail.com>
2026-07-30 22:32:18 +0300

    add current sugar lump type to tooltip

--- LumpSaveScum.js
+++ LumpSaveScum.js
@@ -1,33 +1,59 @@
 console.log("LumpSaveScum loaded");
 
-const Config = {
-    colors: {
-        normal: "#ffffff",
-        bifurcated: "#00aeff",
-        golden: "#ffe600",
-        meaty: "#e92e0d",
-        caramelized: "#e4882c"
-    },
 
-    effects: {
-        glow: true
+const LUMPS = {
+    0: {
+        name: "Normal",
+        color: "#ffffff"
+    },
+    1: {
+        name: "Bifurcated",
+        color: "#00aeff"
+    },
+    2: {
+        name: "Golden",
+        color: "#ffe600"
+    },
+    3: {
+        name: "Meaty",
+        color: "#e92e0d"
+    },
+    4: {
+        name: "Caramelized",
+        color: "#e4882c"
     }
 };
 
-const LUMP_TYPES = {
-    0: "normal",
-    1: "bifurcated",
-    2: "golden",
-    3: "meaty",
-    4: "caramelized"
-};
 
-function update() {
+function updateCounterColor() {
+
     if (!lumpsAmountElement) return;
 
-    lumpsAmountElement.style.color = Config.colors[LUMP_TYPES[Game.lumpCurrentType]];
+    lumpsAmountElement.style.color = LUMPS[Game.lumpCurrentType].color;
 }
+
+
+const originalLumpTooltip = Game.lumpTooltip;
+function updateLumpTooltip() {
+
+    let html = originalLumpTooltip();
+
+    const currentLump = LUMPS[Game.lumpCurrentType];
+
+    html += /*html*/`
+    <div class="line"></div>
+    <div style="margin-bottom: 8px; font: 11px Tahoma, Arial, sans-serif; font-weight: normal; line-height: 1.2; text-align: center;">
+        Current sugar lump:
+        <b style="color: ${currentLump.color};">${currentLump.name}</b>
+    </div>`
+
+    return html;
+}
+ 
+Game.lumpTooltip = updateLumpTooltip;
+
 
 const lumpsAmountElement = document.getElementById("lumpsAmount");
 
-Game.registerHook("draw", update);
+
+Game.registerHook("draw", updateCounterColor);
